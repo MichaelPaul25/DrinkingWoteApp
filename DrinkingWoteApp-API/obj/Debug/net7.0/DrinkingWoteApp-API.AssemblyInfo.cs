60667dcfee8c8426d3d20ccc5703df77f7424879
Michael <wariorstore17@gmail.com>
2024-02-06 15:04:15 +0700

    Update Sln

--- DrinkingWoteApp-API/obj/Debug/net7.0/DrinkingWoteApp-API.AssemblyInfo.cs
+++ DrinkingWoteApp-API/obj/Debug/net7.0/DrinkingWoteApp-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DrinkingWoteApp-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cb9312716e3053926ebe9afe4502ba314d30bc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DrinkingWoteApp-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DrinkingWoteApp-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
