8a1ff1ed064c9edd36f78484a1cea4d43e89f6f7
Michael <wariorstore17@gmail.com>
2024-02-07 16:31:42 +0700

    Add Crew Repositori

---
+++ DrinkingWoteApp-API/Repository/CrewRepository.cs
@@ -0,0 +1,26 @@
+﻿using DrinkingWoteApp_API.Data;
+using DrinkingWoteApp_API.Interfaces;
+using DrinkingWoteApp_API.Models;
+
+namespace DrinkingWoteApp_API.Repository
+{
+    public class CrewRepository : ICrewRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CrewRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<CrewMember> GetAllMembers()
+        {
+            return _context.Crewers.ToList();
+        }
+
+        public CrewMember GetMemberDetails(int id)
+        {
+            return _context.Crewers.Where(c => c.CrewId == id).FirstOrDefault();
+        }
+    }
+}
