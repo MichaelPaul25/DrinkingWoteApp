22b86883d32b95e074eaa9b0ac8a9dfd62f2e935
Michael <wariorstore17@gmail.com>
2024-02-07 16:15:39 +0700

    Add Mapping Profiles
    - To Create Map<Models, ModelsDto>

---
+++ DrinkingWoteApp-API/Helper/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DrinkingWoteApp_API.Dto;
+using DrinkingWoteApp_API.Models;
+
+namespace DrinkingWoteApp_API.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<Consument,  ConsumentDto>();
+            CreateMap<Order, OrderDto>();
+        }
+    }
+}
