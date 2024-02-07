f0eb8718fd5cd0ec6f5e6541000803ff5d79c402
Michael <wariorstore17@gmail.com>
2024-02-07 16:30:04 +0700

    Add ICrew Repository

---
+++ DrinkingWoteApp-API/Interfaces/ICrewRepository.cs
@@ -0,0 +1,10 @@
+﻿using DrinkingWoteApp_API.Models;
+
+namespace DrinkingWoteApp_API.Interfaces
+{
+    public interface ICrewRepository
+    {
+        public ICollection<CrewMember> GetAllMembers();
+        public CrewMember GetMemberDetails(int id);
+    }
+}
