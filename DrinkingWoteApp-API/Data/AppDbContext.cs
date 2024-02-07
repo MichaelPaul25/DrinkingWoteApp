68fba4394d1c4fdaa99fabc264391ebacc947c36
Michael <wariorstore17@gmail.com>
2024-02-07 16:15:04 +0700

    Update AppDbContext
    - Remove Relationship(Confuising the migration)

---
+++ DrinkingWoteApp-API/Data/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using DrinkingWoteApp_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrinkingWoteApp_API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Consument> Consuments { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<CrewMember> Crewers { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<Address> Address { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Perumahan> Perumahans { get; set; }
+        //public DbSet<ConsumentOrder> ConsumentOrders { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<ConsumentOrder>()
+        //        .HasKey(co => new { co.JoinConsumentId, co.JoinOrderId });
+            
+        //    modelBuilder.Entity<ConsumentOrder>()
+        //        .HasOne(p => p.Consument)
+        //        .WithMany(co => co.ConsumenOrders)
+        //        .HasForeignKey(o => o.JoinConsumentId)
+        //        .IsRequired(false)
+        //       .OnDelete(DeleteBehavior.SetNull);
+        //    modelBuilder.Entity<ConsumentOrder>()
+        //       .HasOne(p => p.Order)
+        //       .WithMany(co => co.ConsumentOrders)
+        //       .HasForeignKey(o => o.JoinOrderId)
+        //       .IsRequired(false)
+        //       .OnDelete(DeleteBehavior.SetNull);
+        //}
+
+    }
+}
